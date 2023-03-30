6352126fb7069e9fdc10a20a8ac75b5b5958ecdf
tallesweiler <talles.cavalleiro@gmail.com>
2023-03-30 15:59:43 -0300

    l15

--- test.c
+++ test.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main() {
+
+    int ni,d,c;
+    float nf,u,df,cf;
+
+    while (scanf("%d", &ni)) {
+        nf=ni;
+
+        u=((nf/10)-ni/10)*10;
+
+        df=((nf/100)-ni/100)*10;
+        d=df;
+
+        cf=((nf/1000)-ni/1000)*10;
+        c=cf;
+
+        printf("%d %d %.0f\n", c,d,u);
+    }
+
     return 0;
 }
